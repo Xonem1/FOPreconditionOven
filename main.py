eb5ec1fa8e57a37f0797e5076aaae0cfa2242884
Xonem <beldom13@gmail.com>
2018-03-19 21:24:47 -0700

    pantalla ahora se centra automaticamente

--- main.py
+++ main.py
@@ -18,8 +18,16 @@ class App(tk.Frame):
         tk.Frame.__init__(self, master)
         self.pack()
         self.master.title("Hello World")
-        tk.Label(self, text="Text label").pack()
-
+        tk.Label(self, text="Text label").pack() 
+        self.master.resizable(False,False)
+        
+        self.master.tk_setPalette(background='#ececec')
+        x= int((self.master.winfo_screenwidth()-self.master.winfo_reqwidth())/2)
+        y= int((self.master.winfo_screenheight()-self.master.winfo_reqheight())/2)
+        self.master.geometry("300x200+{}+{}".format(x,y))
+        
+        self.master.config(menu = tk.Menu(self.master))
+        
 # Main body
 if __name__ == '__main__':
     root = tk.Tk()
