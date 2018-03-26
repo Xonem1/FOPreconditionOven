178c137bc9a03c6df85c1afd3d905e31988894f4
Xonem <beldom13@gmail.com>
2018-03-26 10:27:41 -0700

    Signed-off-by: Xonem <beldom13@gmail.com>

--- main.py
+++ main.py
@@ -1,68 +1,113 @@
 #!/usr/bin/env python3 -tt
 """
-Module documentation.
+Autor: Jesus Alberto Esquer Inzunza
+Company: Radiall
+Alias: Xonem
 """
 
 # Imports
 import tkinter as tk
+from tkinter import ttk as ttk
 import platform
-import sys
+
 
 # Global variables
+E = tk.E
+W = tk.W
+N = tk.N
+S = tk.S
+
+TEMP = 125
+TIEMPO = 90
+QTYCABLE = 0.0
+TIPOCABLE = 500
+FONT = "Consolas 20 bold"
+
+#s = ttk.Style()
+#s.configure('my.TButton', font=('Helvetica', 12))
+# TIPOCABLE = ('490', '500')
 
 # Platform.system devuelve el sistema operativo ejemplo:Windows, Linux'
+PLATFORM = platform.system()
 
-platform = platform.system()
-
-# Class declarations
-
-# Function declarations
+# Declaracion de clases
 
 
 class App(tk.Frame):
+    """
+    La clase se encargara de crear una ventana que calculara el numero de
+    ciclos de un horneado, con respecto a una formula y datos constantes.
+    """
     def __init__(self, master):
-        tk.Frame.__init__(self, master)
-        self.pack()
-        self.master.title("Hello World")
-        label1 = tk.Label(self, text="Soy el titulo 1")
-        label1.pack()
-        # tk.Label(self, text="Text label").pack()
-        # self.master.resizable(False, False)
+        """
+        Constructor
+        """
+        self.s = ttk.Style()
+        self.s.configure('my.TButton', font=('Consolas', 20))
+        qty = QTYCABLE
+        tk.Frame.__init__(self)
+        self.grid(sticky=N+S+E+W)
+        top = self.winfo_toplevel()
+        top.rowconfigure(0, weight=1)
+        top.columnconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.columnconfigure(0, weight=1)
 
+        self.configurar_frame()
+        self.maximizar()
+        self.crear_interfaz()
+        self.calculo_ciclo(qty)
+
+
+# Declaracion de Funciones
+    def maximizar(self):
+        """
+        El metodo maximiza la ventana dependiendo del sistema operativo
+        por que el programa se testea en windows y linux.
+        """
+        try:
+            if PLATFORM == "Linux":
+                print("Detectando Sistema Operativo Linux")
+                self.master.attributes('-zoomed', 1)
+            else:
+                print("Detectando Sistema Operativo Windows")
+                self.master.wm_state('zoomed')
+        except tk.TclError as error:
+            print("Error Detectado: ", error)
+            self.destroy()
+            exit(1)
+
+    def configurar_frame(self):
+        """
+        El metodo configura el frame, es parte del constructor
+        """
+        self.master.title("FO Precondicionado - Radiall OBR")
         self.master.tk_setPalette(background='#ececec')
+        self.master.iconbitmap("radiall.ico")
 
-        if platform == "Linux":
-            self.master.attributes('-zoomed', 1)
-        else:
-            self.master.wm_state('zoomed')
+    def crear_interfaz(self):
+        """
+        El metodo creara los botones, entrys y todos los widgets en general
+        que se van a utilizar
+        """
+        self.campo1 = ttk.Entry(self, textvariable=QTYCABLE,
+                                font=FONT, width=20)
+        self.button1 = ttk.Button(self, text="Capturar y calcular",
+                                  style='my.TButton', width=20)
 
-        self.mainMenu(),
-        # self.master.wm_state('zoomed')
-        # self.master.attributes('-zoomed',1)
+        self.campo1.grid(row=0, column=0, padx=30, pady=30,)
+        self.button1.grid(row=1, sticky = N, padx=30, pady=30)
 
-        # self.master.geometry("{}x{}+{}+{}".format(w, h, 0, 0))
+    def calculo_ciclo(self, qty):
+        """
+        El metodo analizara el dato dato y aplicara la formula que se obtuvo
+        de una regresion
+        """
+        print(qty)
+        pass
 
-    def mainMenu(self):
-        self.master.title("FO Precondition")
-        menu = tk.Menu(self.master)
-        self.master.config(menu=menu)
-        file = tk.Menu(menu)
-        file.add_command(label="Exit", command=self.client_exit)
-        menu.add_cascade(label="File", menu=file)
-        edit = tk.Menu(menu)
-        edit.add_command(label="Undo", command=self.undoTest)
-        menu.add_cascade(label="Edit", menu=edit)
-
-    def client_exit(self):
-        self.master.destroy()
-
-    def undoTest(self):
-        
-# Main body
 
 if __name__ == '__main__':
-
-    root = tk.Tk()
-    app = App(root)
-    app.mainloop()
-
+    ROOT = tk.Tk()
+    APP = App(ROOT)
+    APP.mainloop()
